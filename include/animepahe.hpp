2c29cfeb3c409629eea1dc3faeff56925e2f816c
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-07-27 19:34:58 +0530

    Add Animepahe class header file with initial method declarations

---
+++ include/animepahe.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef ANIMEPAHE_HPP
+#define ANIMEPAHE_HPP
+
+#include <cpr/cpr.h>
+#include <map>
+#include <vector>
+#include <string>
+
+namespace AnimepaheCLI
+{
+    class Animepahe
+    {
+    private:
+        cpr::Header getHeaders(const std::string &link);
+        std::map<std::string, std::string> fetch_episode(const std::string &link);
+        std::vector<std::string> fetch_series(const std::string &link);
+        void extract_link_metadata(const std::string &link, bool isSeries);
+        std::vector<std::map<std::string, std::string>> extract_link_content(
+            const std::string &link,
+            const std::vector<int> &episodes,
+            bool isSeries,
+            bool isAllEpisodes
+        );
+    public:
+        void extractor(
+            bool isSeries,
+            const std::string &link,
+            bool isAllEpisodes,
+            const std::vector<int> &episodes,
+            bool exportLinks = false,
+            bool createZip = false
+        );
+    };
+}
+
+#endif
