f43bbb42934c41c947aa767029f457369c94c651
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-08-08 01:52:32 +0530

    update return type of extract_link_metadata

--- include/animepahe.hpp
+++ include/animepahe.hpp
@@ -17,7 +17,7 @@ namespace AnimepaheCLI
         std::map<std::string, std::string> fetch_episode(const std::string &link, const int &targetRes);
         int get_series_episode_count(const std::string& link);
         std::vector<std::string> fetch_series(const std::string &link, const int epCount, bool isAllEpisodes, const std::vector<int> &episodes);
-        void extract_link_metadata(const std::string &link, bool isSeries);
+        std::string extract_link_metadata(const std::string &link, bool isSeries);
         std::vector<std::map<std::string, std::string>> extract_link_content(
             const std::string &link,
             const std::vector<int> &episodes,
