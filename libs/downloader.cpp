8625b1347687250dc448bf8825c58ca3a685b2a8
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-08-07 14:03:57 +0530

    implement downoad logic

---
+++ libs/downloader.cpp
@@ -0,0 +1,141 @@
+#include "downloader.hpp"
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <regex>
+
+Downloader::Downloader(const std::vector<std::string> &urls) : urls_(urls) {}
+const char *CLEAR_LINE = "\033[2K"; // Clear entire line
+const char *CURSOR_START = "\r";    // Return to start of line
+
+void Downloader::setDownloadDirectory(const std::string &dir)
+{
+    download_dir_ = dir;
+    if (!std::filesystem::exists(download_dir_))
+    {
+        std::filesystem::create_directory(download_dir_);
+    }
+}
+
+void Downloader::startDownloads()
+{
+    fmt::print("\n");
+    for (const auto &url : urls_)
+    {
+        std::string filename = extractFilename(url);
+        std::string filepath = download_dir_ + "/" + filename;
+
+        fmt::print("\n * Downloading : ");
+        fmt::print(fmt::fg(fmt::color::cyan), fmt::format("{}\n", filename));
+        if (!downloadFile(url, filepath))
+        {
+            fmt::print("\n * Download failed : {} , skipping to next.\n", url);
+            std::filesystem::remove(filepath);
+            continue;
+        }
+        else
+        {
+            fmt::print("{}{}", CLEAR_LINE, CURSOR_START);
+            fmt::print("{}{}", CLEAR_LINE, CURSOR_START);
+            fmt::print("{}{}", CLEAR_LINE, CURSOR_START);
+            fmt::print("{}{}", CLEAR_LINE, CURSOR_START);
+            fmt::print(" * DL (");
+            fmt::print(fmt::fg(fmt::color::lime_green), "OK");
+            fmt::print(")     : {}\n", filename);
+        }
+    }
+}
+
+std::string Downloader::extractFilename(const std::string &url) const
+{
+    /* Try to find "?file=" followed by the filename */
+    std::regex filenamePattern(R"([\?&]file=([^&]+))");
+    std::smatch match;
+
+    if (std::regex_search(url, match, filenamePattern) && match.size() > 1)
+    {
+        return match[1];
+    }
+
+    /* If not found, generate a unique filename with timestamp */
+    auto now = std::chrono::system_clock::now();
+    auto now_time = std::chrono::system_clock::to_time_t(now);
+    auto ms_part = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
+
+    std::ostringstream oss;
+    oss << "file_" << std::put_time(std::localtime(&now_time), "%Y%m%d_%H%M%S") << "_" << ms_part << ".mp4";
+    return oss.str();
+}
+
+std::string formatTime(double totalSeconds) {
+    int seconds = static_cast<int>(std::round(totalSeconds)); // Round to nearest second
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int secs = seconds % 60;
+
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << hours << ":"
+        << std::setw(2) << std::setfill('0') << minutes << ":"
+        << std::setw(2) << std::setfill('0') << secs;
+
+    return oss.str();
+}
+
+std::string formatSpeedMB(double speedKBps) {
+    double mbps = speedKBps / (1024.0 * 1024.0);
+
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2)
+        << std::setw(4) << mbps << " MB/s";
+
+    return oss.str();
+}
+
+bool Downloader::downloadFile(const std::string &url, const std::string &filepath)
+{
+    std::ofstream outfile(filepath, std::ios::binary);
+    if (!outfile.is_open())
+    {
+        fmt::print("\n * Failed to open file : {}\n", filepath);
+        return false;
+    }
+
+    auto start_time = std::chrono::steady_clock::now();
+    cpr::Response r = cpr::Get(
+        cpr::Url{url},
+        cpr::WriteCallback{
+            [&outfile](std::string data, intptr_t)
+            {
+                outfile.write(data.data(), data.size());
+                return true;
+            }},
+        cpr::ProgressCallback{[&start_time](size_t downloadTotal, size_t downloadNow, size_t, size_t, intptr_t)
+        {
+            if (downloadTotal > 0)
+            {
+                double progress = static_cast<double>(downloadNow) / downloadTotal * 100.0;
+                auto now = std::chrono::steady_clock::now();
+                auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - start_time).count();
+                if (elapsed > 0)
+                {
+                    double speed = static_cast<double>(downloadNow) / elapsed;
+                    double remaining = (downloadTotal - downloadNow) / speed;
+                    double kbSpeed = speed / 1024;
+                    std::cout << "\r * Progress: " << std::fixed
+                    << std::setprecision(2) << progress
+                    << "% ETA: " << formatTime(remaining) << "S | " << formatSpeedMB(speed) << std::flush;
+                }
+            }
+            return true;
+        }
+    });
+
+    outfile.close();
+    return r.status_code == 200;
+}
